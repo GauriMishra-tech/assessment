f6bb77fe89d119403617f30d3a21898c9c191231
Gauri Mishra <gauri.mishra@pl.abb.com>
2026-09-05 23:37:46 +0530

    new changes added

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,26 @@
-import { defineConfig } from "cypress";
+ import { defineConfig } from "cypress";
 
 export default defineConfig({
-  allowCypressEnv: false,
-
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+  viewportWidth: 1920,
+  viewportHeight: 1180,
+  // viewportWidth: 320,
+  // viewportHeight: 568,
+  defaultCommandTimeout: 12000,
+  numTestsKeptInMemory: 0,
+  pageLoadTimeout: 60000,
+  responseTimeout: 45000,
+  requestTimeout: 45000,
+  retries: {
+    runMode: 2,
+    openMode: 2,
   },
-});
+  env: {
+    baseUrl: "https://demoqa.com/forms",
+  },
+  e2e: {
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Default pattern
+    setupNodeEvents(on, config) {
+      //Implement Code
+    },
+  }
+})
--- cypress/e2e/elements.cy.ts
+++ cypress/e2e/elements.cy.ts
@@ -1,10 +1,10 @@
+
 describe('Validate elements', () => {
     beforeEach(() => {
-        cy.visit('https://demoqa.com/forms'),
+        cy.visit(Cypress.env("baseUrl")),
         cy.get('span').contains('Elements').click()
     }),
     it('ValidateText', () => {
-        
         cy.get('span').contains('Text Box').click(),
         cy.get('input[placeholder="Full Name"]').type('Test Full Name'),
         cy.get('input[placeholder="name@example.com"]').type('test@gmail.com'),
@@ -15,10 +15,8 @@ describe('Validate elements', () => {
         cy.get('p').contains('test@gmail.com').should('be.visible'),
         cy.get('p').contains('Test Current Address').should('be.visible'),
         cy.get('p').contains('Test Permanent Address').should('be.visible')
-    }),
+    })
     it('Validate CheckBox', () => {
-        //cy.visit('https://demoqa.com/forms'),
-        //cy.get('span').contains('Elements').click(),
         cy.get('span').contains('Check Box').click(),
         cy.get('.rc-tree-switcher.rc-tree-switcher_close').click(),
         cy.get('.rc-tree-checkbox[aria-label="Select Home"]').click(),
@@ -28,5 +26,41 @@ describe('Validate elements', () => {
         cy.get('.rc-tree-checkbox[aria-label="Select Downloads"]').click(),
         cy.get('.rc-tree-checkbox[aria-label="Select Downloads"]').should('have.attr','aria-checked','false'),
         cy.get('span').contains('downloads').should('not.exist')
+    })
+    it('Validate Radio Button', () => {
+        cy.get('span').contains('Radio Button').click(),
+        cy.get('div.mb-3').contains('Do you like the site?').should('be.visible'),
+        cy.get('label[for="yesRadio"]').click(),
+        cy.get('p').contains('You have selected').should('be.visible'),
+        cy.get('span').contains('Yes').should('be.visible'),
+        cy.get('label[for="impressiveRadio"]').click(),
+        cy.get('p').contains('You have selected ').should('be.visible'),
+        cy.get('span').contains('Impressive').should('be.visible'),
+        cy.get('label[for="noRadio"]').siblings().should('have.attr','disabled')
+    }),
+    it('Validate Web Tables', () => {
+        cy.get('span').contains('RWeb Tables').click(),
+        cy.get('button').contains('Add').click(),
+        cy.get('input[placeholder="First Name"]').type('Sam'),
+        cy.get('input[placeholder="Last Name"]').type('Smith'),
+        cy.get('input[placeholder="name@example.com"]').type('test@gmail.com'),
+        cy.get('input[placeholder="Age"]').type('25'),
+        cy.get('input[placeholder="Salary"]').type('10000'),
+        cy.get('input[placeholder="Department"]').type('Sales'),
+        cy.get('button').contains('Submit').click(),
+        cy.get('table>tbody>tr').should('have.length',4),
+        cy.get('input[placeholder="Type to search"]').type('Sales'),
+        cy.get('table>tbody>tr').should('have.length',1),
+        cy.get('span[title="Edit"]').click(),
+        cy.get('input[placeholder="Department"]').type('Finance'),
+        cy.get('table>tbody>tr').should('have.length',0)
+    })
+    it('Validate Buttons', () => {
+        cy.get('span').contains('Double Click Me').dblclick(),
+        cy.get('p').contains('You have done a double click').should('be.visible'),
+        cy.get('span').contains('Right Click Me').rightclick(),
+        cy.get('p').contains('You have done a right click').should('be.visible')
+        cy.get('span').contains('Click Me').click(),
+        cy.get('p').contains('You have done a dynamic click').should('be.visible')
     })
 })
